bd44d3318aeec7298c7d7edf17c74a6d2ed12b95
lowlyHero <e.nunn1009@gmail.com>
2019-09-30 19:09:56 -0400

    still working on functionality. getting there

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 import './styles/App.scss';
+
+const todoData = [
+  {
+    task: '',
+    id: Date.now(),
+    completed: false
+  }
+]
 
 class App extends Component {
  constructor() {
    super();
    this.state= {
-     toDo: ''
-   }
+     tasks: todoData,
+   };
  }
+
+
 
   render() {
     return (
       <div className='App'>
-        <h2>Welcome to your Todo App!</h2>
+        <h2 className='app-title'>Todo App</h2>
+        <div>
+          <TodoForm />
+        </div>
       </div>
     );
   }
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    return (
+        <div className='todo-list'>
+            {props.tasks.map(todo => (
+                <Todo key={todo.id} todo={todo} />
+            ))}
+        </div>
+    )
+}
+
+export default TodoList;
